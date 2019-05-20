a184b86748ba22ce963a44193bda9312c9483ac9
readerextrodinaire <45886619+readerextrodinaire@users.noreply.github.com>
2019-05-19 19:41:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-function nav() {
-    document.getElementById("mydropdown").classList.toggle("show");
-    document.getElementById("mydropdown").style.marginLeft = ("-85px");
-    document.getElementById("mydropdown").style.marginTop = ("-250px");
+function schools() {
+    function stuy() {
+        var stuy = document.getElementById("stuy");
+        stuy.textContent = 'Mision Statement:"Stuyvesant High School has been a symbol of excellence in education for over a century. Our mission is to continue and enhance that commitment by providing an environment which will nurture and enhance the special academic talents of the students admitted to Stuyvesant. The educational heritage of Stuyvesant is deeply rooted in the tradition of Science, Mathematics and Technology. This has been the foundation of our educational success and must remain the cornerstone of our educational program. Within this context, the goal of this institution is to instill the intellectual, moral and humanistic values necessary for each child to achieve his/her maximum potential as a student and as a caring citizen of the world."';
+    };
+    stuy;
+    function bronx() {
+        var bronx = document.getElementById("bronx");
+        bronx.textContent ='Mission Statment:"We are the ideal school for all who seek a rigorous college preparatory program and wish to learn in an environment committed to educational excellence."';
+    }
+    bronx;
 }
-function one() {
-    document.getElementById("drop2").classList.toggle("show");
-}
-function two() {
-    document.getElementById("drop3").classList.toggle("show");
-}
-nav();
-one();
-two();
+schools;
